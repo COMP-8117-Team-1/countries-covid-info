f2fcd18357b5b02f086898552af489696c08602e
Adaobi <adamichelllle@gmail.com>
2020-11-24 13:04:48 +0100

    Implement search feature

--- app.js
+++ app.js
@@ -2,35 +2,84 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { logger } = require('./utils');
+const axios = require('axios');
 
 const app = express();
-
 const router = express.Router();
 
 app.set('view engine', 'ejs');
 app.use(logger)
 app.use(bodyParser.urlencoded({extended: false}));
 
+/**
+ * Load static resources like the landing page and the site assets like css, js and images
+ */
 app.use(express.static('public'));
-
 app.use('/', express.static(path.join(__dirname + 'public')));
+
+/**
+ * About route. Renders about page
+ */
 app.get('/about', (req, res) => {
-    res.render('pages/app');
+    res.render('pages/about');
 });
 
-
+/**
+ * App route. Render the app page with the search form.
+ */
 router.get('/', (req, res) => {
     res.render('pages/app')
 });
 
+/**
+ * Search route: Retrieves data from the APIs for the request query "country".
+ * Renders a page containing the info as a response
+ * @param req
+ * @param res
+ */
 router.get('/search', (req, res) => {
-    res.render('pages/search')
+    const country = req.query.country;
+
+    let countriesAPIUrl = "https://restcountries.eu/rest/v2/name/" + country + "?fullText=true";
+    let covidAPIUrl = "https://api.covid19api.com/total/country/" + country;
+
+    let countriesAPIRequest = axios.get(countriesAPIUrl) ;
+    let covidAPIRequest = axios.get(covidAPIUrl);
+
+    axios
+        .all([countriesAPIRequest, covidAPIRequest])
+        .then(
+            axios.spread((...responses) => {
+                const countriesAPIResponse = responses[0].data;
+                const covidAPIResponse = responses[1].data;
+                const currentCovidData = covidAPIResponse[covidAPIResponse.length - 1];
+
+                res.status(200);
+                res.render('pages/search', {
+                    country: req.query.country,
+                    countryInfo: countriesAPIResponse[0],
+                    countryCovidData: currentCovidData
+                });
+            })
+        )
+        .catch(errors => {
+            res.status(404).render('pages/error404');
+        });
 });
 
+/**
+ * Use router for specified path
+ */
 app.use('/app', router);
 
+/**
+ * Handle invalid routes
+ */
+app.get('*', (req, res) => res.status(404).render('pages/error404'));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log("Server is running at port " + port);
 });
+
+module.exports = app;
--- utils/logger.js
+++ utils/logger.js
@@ -6,18 +6,22 @@ const logger = (req, res, next) => {
     const date = moment().format("DD/MM/YYYY hh:MM:SS A");
     const method = req.method;
     const request = req.url;
-    const status = res.statusCode;
+    //const status = res.statusCode;
 
     const start = process.hrtime();
     const duration = convertHrtime(process.hrtime(start)).milliseconds;
 
-    let log = `[${date}] | ${method}:${request} | Status: ${status} | Request Duration: ${duration} ms`;
+    res.on('finish', function() {
+      let statusCode = this.statusCode;
+      let errorMessage = statusCode != 200 ? `| Error Message: ${this.statusMessage}` : "";
+      let log = `[${date}] | ${method}:${request} | Status: ${statusCode} | Request Duration: ${duration} ms ${errorMessage}`;
 
-    fs.appendFile("logs.txt", log + "\n", err => {
-        if (err) {
-          console.log(err);
-        }
-    });
+      fs.appendFile("logs.txt", log + "\n", err => {
+          if (err) {
+            console.log(err);
+          }
+      });
+    })
     next();
 }
 
